01efc6531df23931e8f69c335e4fedfbb5e9fd1d
toyotarou <toyodahideyuki4819@gmail.com>
2024-08-28 21:09:17 +0900

    commit 20240828

--- lib/screens/components/pages/yearly_credit_category_list_page.dart
+++ lib/screens/components/pages/yearly_credit_category_list_page.dart
@@ -1,11 +1,10 @@
+import 'package:credit_note/collections/credit_detail.dart';
+import 'package:credit_note/collections/credit_item.dart';
+import 'package:credit_note/extensions/extensions.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:isar/isar.dart';
-
-import '../../../collections/credit_detail.dart';
-import '../../../collections/credit_item.dart';
-import '../../../extensions/extensions.dart';
 
 class YearlyCreditCategoryListPage extends StatefulWidget {
   const YearlyCreditCategoryListPage(
@@ -21,10 +20,28 @@ class YearlyCreditCategoryListPage extends StatefulWidget {
   final List<CreditDetail>? creditDetailList;
 
   @override
-  State<YearlyCreditCategoryListPage> createState() => _YearlyCreditCategoryListPageState();
+  State<YearlyCreditCategoryListPage> createState() =>
+      _YearlyCreditCategoryListPageState();
 }
 
-class _YearlyCreditCategoryListPageState extends State<YearlyCreditCategoryListPage> {
+class _YearlyCreditCategoryListPageState
+    extends State<YearlyCreditCategoryListPage> {
+  List<CreditDetail> cdList = [];
+
+  ///
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.creditDetailList != null) {
+      cdList = widget.creditDetailList!
+          .where((element) =>
+              element.yearmonth.split('-')[0].toInt() == widget.date.year)
+          .toList();
+    }
+  }
+
+  ///
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -53,175 +70,154 @@ class _YearlyCreditCategoryListPageState extends State<YearlyCreditCategoryListP
 
   ///
   Widget _displayYearlyCreditCategoryList() {
-    final monthSumMap = <int, int>{};
-    widget.creditItemList?.forEach((element) {
-      for (var i = 1; i <= 12; i++) {
-        final filtered = widget.creditDetailList
-            ?.where((element2) => element2.yearmonth == '${widget.date.year}-${i.toString().padLeft(2, '0')}')
-            .toList();
+    final list = <Widget>[];
 
-        var sum = 0;
-        filtered?.forEach((element) {
-          sum += element.creditDetailPrice;
+    var underWidget = const Column();
+
+    if (widget.creditItemList != null) {
+      final map = <int, Map<String, int>>{};
+
+      for (var i = 1; i <= 12; i++) {
+        final map2 = <String, List<int>>{};
+
+        widget.creditItemList!.forEach((element2) => map2[element2.name] = []);
+
+        cdList.forEach((element) {
+          final widgetYearmonth =
+              '${widget.date.year}-${i.toString().padLeft(2, '0')}';
+
+          if (element.yearmonth == widgetYearmonth) {
+            widget.creditItemList!.forEach((element2) {
+              if (element.creditDetailItem == element2.name) {
+                map2[element2.name]?.add(element.creditDetailPrice);
+              }
+            });
+          }
         });
 
-        monthSumMap[i] = sum;
+        final map3 = <String, int>{};
+
+        widget.creditItemList!.forEach((element4) => map3[element4.name] = 0);
+
+        map2.forEach((key, value) {
+          var sum = 0;
+          value.forEach((element3) => sum += element3);
+          map3[key] = sum;
+        });
+
+        map[i] = map3;
       }
-    });
 
-    final creditCategoryMap = <String, List<String>>{};
+      final list2 = <Widget>[const SizedBox(width: 140)];
 
-    widget.creditItemList?.forEach((element) {
-      creditCategoryMap[element.name] = [];
-    });
-
-    widget.creditItemList?.forEach((element) {
       for (var i = 1; i <= 12; i++) {
-        final filtered = widget.creditDetailList
-            ?.where((element2) => element2.yearmonth == '${widget.date.year}-${i.toString().padLeft(2, '0')}')
-            .toList();
+        final list3 = <Widget>[];
 
-        //=====================================
         var sum = 0;
-        if (filtered!.isNotEmpty) {
-          final filtered2 = filtered.where((element3) => element.name == element3.creditDetailItem).toList();
 
-          if (filtered2.isNotEmpty) {
-            filtered2.forEach((element4) => sum += element4.creditDetailPrice);
-          }
-        }
-        //=====================================
-
-        creditCategoryMap[element.name]?.add(sum.toString().toCurrency());
-      }
-    });
-
-    const valueDevide = 5;
-
-    final list = <Widget>[
-      Row(
-        children: [
-          Container(
-            width: context.screenSize.width / 6,
-            margin: const EdgeInsets.symmetric(vertical: 5),
-            padding: const EdgeInsets.symmetric(vertical: 3),
-            alignment: Alignment.topRight,
-            child: const Text('', style: TextStyle(fontSize: 10)),
-          ),
-          const SizedBox(width: 10),
-          for (var i = 0; i < 12; i++) ...[
+        if (DateTime(widget.date.year, i).isBefore(DateTime.now())) {
+          list3.add(
             Container(
-              width: context.screenSize.width / valueDevide,
-              padding: const EdgeInsets.all(1),
-              margin: const EdgeInsets.symmetric(vertical: 3, horizontal: 1),
+              width: 100,
+              height: 30,
               alignment: Alignment.center,
-              child: Text((i + 1).toString()),
+              child: Text(i.toString().padLeft(2, '0')),
             ),
-          ],
-        ],
-      ),
-    ];
+          );
 
-    final creditItemColorMap = <String, String>{};
-    widget.creditItemList?.forEach((element) {
-      creditItemColorMap[element.name] = element.color;
-    });
-
-    widget.creditItemList?.forEach((element) {
-      final lineColor = (creditItemColorMap[element.name] != null && creditItemColorMap[element.name] != '')
-          ? creditItemColorMap[element.name]
-          : '0xffffffff';
-
-      var sum = 0;
-
-      if (creditCategoryMap[element.name] != null) {
-        for (var i = 0; i < 12; i++) {
-          if (creditCategoryMap[element.name]![i].isNotEmpty) {
-            sum += creditCategoryMap[element.name]![i].replaceAll(',', '').toInt();
-          }
-        }
-      }
-
-      if (sum > 0) {
-        final list2 = <Widget>[
-          Container(
-            width: context.screenSize.width / 6,
-            margin: const EdgeInsets.symmetric(vertical: 5),
-            padding: const EdgeInsets.symmetric(vertical: 3),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Color(lineColor!.toInt()).withOpacity(0.2), borderRadius: BorderRadius.circular(10)),
-            child: FittedBox(child: Text(element.name, style: const TextStyle(fontSize: 10))),
-          ),
-          const SizedBox(width: 10),
-        ];
-
-        if (creditCategoryMap[element.name] != null) {
-          for (var i = 0; i < 12; i++) {
-            if (creditCategoryMap[element.name]![i].isNotEmpty) {
-              list2.add(
-                Container(
-                  width: context.screenSize.width / valueDevide,
-                  padding: const EdgeInsets.all(1),
-                  margin: const EdgeInsets.symmetric(vertical: 3, horizontal: 1),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.transparent, width: 2),
-                    color: Color(lineColor.toInt()).withOpacity(0.1),
-                  ),
-                  alignment: Alignment.topRight,
-                  child: Text(creditCategoryMap[element.name]![i]),
-                ),
-              );
-            }
-          }
-        }
-
-        list2.add(
-          Row(
-            children: [
-              const SizedBox(width: 10),
-              Container(
-                width: context.screenSize.width / 6,
-                margin: const EdgeInsets.symmetric(vertical: 3, horizontal: 1),
-                padding: const EdgeInsets.symmetric(vertical: 3),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Color(lineColor.toInt()).withOpacity(0.2), borderRadius: BorderRadius.circular(10)),
-                child: Text(element.name, style: const TextStyle(fontSize: 10)),
+          widget.creditItemList!.forEach((element5) {
+            list3.add(Container(
+              width: 100,
+              height: 24,
+              margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Color(element5.color.toInt()).withOpacity(0.1),
               ),
-            ],
-          ),
-        );
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        alignment: Alignment.topRight,
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Text(
+                          (map[i] != null && map[i]![element5.name] != null)
+                              ? map[i]![element5.name]!.toString().toCurrency()
+                              : '',
+                        ),
+                      ),
+                      Text(
+                        i.toString().padLeft(2, '0'),
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ));
 
-        list.add(Row(children: list2));
-      }
-    });
+            sum += (map[i] != null && map[i]![element5.name] != null)
+                ? map[i]![element5.name]!
+                : 0;
+          });
 
-    list.add(
-      Row(
-        children: [
-          Container(
-            width: context.screenSize.width / 6,
-            margin: const EdgeInsets.symmetric(vertical: 5),
-            padding: const EdgeInsets.symmetric(vertical: 3),
-            alignment: Alignment.topRight,
-            child: const Text('', style: TextStyle(fontSize: 10)),
-          ),
-          const SizedBox(width: 10),
-          for (var i = 0; i < 12; i++) ...[
-            Container(
-              width: context.screenSize.width / valueDevide,
-              padding: const EdgeInsets.all(1),
-              margin: const EdgeInsets.symmetric(vertical: 3, horizontal: 1),
-              alignment: Alignment.topRight,
-              child: Text((monthSumMap[(i + 1)] != null) ? monthSumMap[(i + 1)].toString().toCurrency() : '0'),
+          list3.add(Container(
+            width: 100,
+            height: 24,
+            margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+            padding: const EdgeInsets.all(2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.topRight,
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Text(sum.toString().toCurrency()),
+                ),
+              ],
             ),
-          ],
+          ));
+        }
+
+        list2.add(Column(children: list3));
+      }
+
+      list.add(Row(children: list2));
+
+      underWidget = Column(
+        children: [
+          const SizedBox(width: 100, height: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widget.creditItemList!.map((e) {
+              return Container(
+                width: 120,
+                height: 24,
+                margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  color: Color(e.color.toInt()).withOpacity(0.1),
+                ),
+                child: Text(e.name),
+              );
+            }).toList(),
+          ),
+        ],
+      );
+    }
+
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          underWidget,
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Column(children: list),
+          ),
         ],
       ),
     );
-
-    return SingleChildScrollView(
-        scrollDirection: Axis.horizontal, child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: list));
   }
 }
